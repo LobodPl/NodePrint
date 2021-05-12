f2bed6ad5b8eb03a7e34441e24d6bf24e729f455
Lobod <konta.lobod@o2.pl>
2021-05-12 22:51:15 +0200

    Added filestream

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const socket = require("socket.io");
+const socketStream = require('socket.io-stream');
 const prompt = require('prompt');
 const db = require("./modules/databaseworker")
-let connectedNodes = []
+const fs = require('fs');
 // App setup
 const PORT = 3000;
+var stream = socketStream.createStream();
 const app = express();
 const server = app.listen(PORT, function () {
 	console.log(`Main app is listening on port ${PORT}`);
@@ -15,50 +17,73 @@ const server = app.listen(PORT, function () {
 app.use(express.static("public"));
 
 // Socket setup
-const io = socket(server);
+const io = socket(server).of("/mirror");
 
 io.on("connection", function (socket) {
 	console.log("Mirror node is connected");
-	socket.on("log",console.log)
+	socket.on("log", console.log)
 	socket.on("nodeId", (nodeId) => {
 		const secret = db.getSecretById(nodeId);
 		if (secret != 0) {
 			socket.emit("apiKey", secret);
-			const messageTemplate={"apiKey":secret}
+			const messageTemplate = { "apiKey": secret }
 			let message = messageTemplate;
-			socket.emit("getPortList",message);
-			socket.on("ports",(portList)=>{
+			socket.emit("getPortList", message);
+			socket.on("ports", (portList) => {
 				console.log(portList);
 				prompt.start();
-				prompt.get("index",(err,res)=>{
+				prompt.get("index", (err, res) => {
 					message = messageTemplate;
-					if(res.index == get){
-						socket.emit("getPortList",message);
-					}else{
-						message.port = JSON.parse(portList)[res.index].path;
-						socket.emit("setComPort",message);
-						socket.emit("connectPrinter",message);
-						getGcode(messageTemplate,socket);
+					if (res.index == "get") {
+						socket.emit("getPortList", message);
 					}
+					else if (res.index == "file") {
+						sendFile("test.txt", messageTemplate, socket)
+					} else {
+						message.port = JSON.parse(portList)[res.index].path;
+						socket.emit("setComPort", message);
+						socket.emit("connectPrinter", message);
+						getGcode(messageTemplate, socket);
+					}
+
 				});
 			});
 		}
 		else {
 			socket.emit("pairRequest");
-			socket.once("pairResponse", (responsedata) =>{
-				db.storeSecretWithId(responsedata.id,responsedata.secret);
+			socket.once("pairResponse", (responsedata) => {
+				db.storeSecretWithId(responsedata.id, responsedata.secret);
 			})
 		}
 	})
 });
 
-function getGcode(messageTemplate,socket){
-	prompt.get("Gcode",(err,res)=>{
+function getGcode(messageTemplate, socket) {
+	prompt.get("Gcode", (err, res) => {
 		message = messageTemplate;
 		message.command = res["Gcode"];
-		socket.emit("command",message);
+		socket.emit("command", message);
 		setTimeout(() => {
-			getGcode(messageTemplate,socket);
-		}, 100); 
+			getGcode(messageTemplate, socket);
+		}, 100);
 	});
+}
+
+function sendFile(filename, message, socket) {
+	message.data = makeid(20);
+	socket.emit("fileTransfer",message);
+	socketStream(socket).once(message.data, function (stream) {
+		fs.createReadStream(filename).pipe(stream);
+	});
+}
+
+function makeid(length) {
+	var result = [];
+	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result.push(characters.charAt(Math.floor(Math.random() *
+			charactersLength)));
+	}
+	return result.join('');
 }
--- mirror/mirrorservice.js
+++ mirror/mirrorservice.js
@@ -1,16 +1,12 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const readline = require("readline");
 const io = require("socket.io-client");
+const socketStream = require('socket.io-stream');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
+const lineByLine = require('n-readlines');
 
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
 class PrinterWorker {
 	isDataReceived = false;
 	commandEnquedCount = 0;
@@ -23,12 +19,14 @@ class PrinterWorker {
 	static apiKey = null;
 	static appId = null;
 	static socket = null;
+	static stream = null;
+	state = "idle";
 
 	constructor() {
 		this.loadConfig();
 		PrinterWorker.socket.on("connect", () => {
 			PrinterWorker.socket.emit("nodeId", PrinterWorker.appId);
-			if(PrinterWorker.apiKey == null){
+			if (PrinterWorker.apiKey == null) {
 				PrinterWorker.socket.once("pairRequest", () => {
 					this.pairRequest();
 				});
@@ -43,29 +41,38 @@ class PrinterWorker {
 	}
 
 	prepareSocket() {
-		if(! this.prepared){
-
-			PrinterWorker.socket.on("disconnect",()=>{
-				if (this.port.isOpen){
+		if (!this.prepared) {
+			PrinterWorker.socket.on("disconnect", () => {
+				if (this.port != null && this.port.isOpen) {
 					this.port.close();
 				}
 			});
 
+
 			PrinterWorker.socket.on("command", (message) => {
-				this.PermissionProxy(message.apiKey,()=>{this.serialWrite(message.command)});
+				this.PermissionProxy(message.apiKey, () => { this.serialWrite(message.command) });
 			});
-	
+
 			PrinterWorker.socket.on("setComPort", (message) => {
-				this.PermissionProxy(message.apiKey,()=>{this.setComPort(message.port);});
+				this.PermissionProxy(message.apiKey, () => { this.setComPort(message.port); });
 			});
-	
+
 			PrinterWorker.socket.on("connectPrinter", (message) => {
-				this.PermissionProxy(message.apiKey,()=>{this.connect();});
+				this.PermissionProxy(message.apiKey, () => { this.connect(); });
 			});
-	
+
 			PrinterWorker.socket.on("getPortList", (message) => {
-				this.PermissionProxy(message.apiKey,()=>{this.portList();});
+				this.PermissionProxy(message.apiKey, () => { this.portList(); });
 			});
+
+			PrinterWorker.socket.on("getState", (message) => {
+				this.PermissionProxy(message.apiKey, () => { this.getState(); });
+			});
+
+			PrinterWorker.socket.on("fileTransfer", (message) => {
+				this.PermissionProxy(message.apiKey, () => { this.fileTransfer(message.data); });
+			});
+
 			this.prepared = true;
 		}
 
@@ -75,73 +82,77 @@ class PrinterWorker {
 			const data = fs.readFileSync('config.json')
 			let parsedConfig = JSON.parse(data);
 			PrinterWorker.apiKey = parsedConfig.apiKey;
-			PrinterWorker.socket = io(parsedConfig.nodeAddress);
+			PrinterWorker.socket = io(parsedConfig.nodeAddress+"/mirror");
 			PrinterWorker.comPort = parsedConfig.comPort;
 			PrinterWorker.appId = parsedConfig.nodeId;
 		} else {
-			const blankConfig = { "nodeId": uuidv4(), "apiKey":null, "comPort": null, "nodeAddress": "ws://localhost:3000" };
+			const blankConfig = { "nodeId": uuidv4(), "apiKey": null, "comPort": null, "nodeAddress": "ws://localhost:3000" };
 			fs.writeFileSync("config.json", JSON.stringify(blankConfig));
 			console.log("Proszę uzupełnic config.cfg");
 			process.exit();
 		}
 	}
-	pairRequest(){
-		if(PrinterWorker.apiKey == null){
+	pairRequest() {
+		if (PrinterWorker.apiKey == null) {
 			const token = uuidv4();
 			PrinterWorker.apiKey = bcrypt.hashSync(token, 10);
 			const data = fs.readFileSync('config.json')
 			let parsedConfig = JSON.parse(data);
 			parsedConfig.apiKey = PrinterWorker.apiKey;
 			fs.writeFileSync("config.json", JSON.stringify(parsedConfig));
-			PrinterWorker.socket.emit("pairResponse",{"id":PrinterWorker.appId,"secret":token})
+			PrinterWorker.socket.emit("pairResponse", { "id": PrinterWorker.appId, "secret": token })
 			PrinterWorker.socket.emit("nodeId", PrinterWorker.appId);
 
 		}
 	}
 	setComPort(comPort) {
-		console.log("[CFG] >> COM port set to :"+comPort)
-		PrinterWorker.comPort = comPort;
-		const data = fs.readFileSync('config.json')
-		let parsedConfig = JSON.parse(data);
-		parsedConfig.comPort = PrinterWorker.comPort;
-		fs.writeFileSync("config.json", JSON.stringify(parsedConfig));
+		if (this.state == "idle") {
+			console.log("[CFG] >> COM port set to :" + comPort);
+			PrinterWorker.comPort = comPort;
+			const data = fs.readFileSync('config.json');
+			let parsedConfig = JSON.parse(data);
+			parsedConfig.comPort = PrinterWorker.comPort;
+			fs.writeFileSync("config.json", JSON.stringify(parsedConfig));
+		}
 
 	}
 	connect() {
-		this.port = new SerialPort(PrinterWorker.comPort, { baudRate: this.baudrates[this.baudratesUsedIndex] });
-		this.parser = this.port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
+		if (this.state == "idle" && (this.port == null || !this.port.isOpen)) {
+			this.port = new SerialPort(PrinterWorker.comPort, { baudRate: this.baudrates[this.baudratesUsedIndex] });
+			this.parser = this.port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
 
-		this.port.on("open", () => {
-			console.log('[Serial] Checking Baudrate');
-			setTimeout(() => {
-				if (!this.isDataReceived) {
-					if (this.baudratesUsedIndex + 1 < this.baudrates.length) {
-						this.baudratesUsedIndex++;
-						this.port.update({ baudRate: this.baudrates[this.baudratesUsedIndex] });
-						this.port.flush();
-						this.parser._flush((x) => { });
-						setTimeout(() => {
-							this.port.close();
-							setTimeout(() => { this.port.open(); }, 200);
-						}, 200);
+			this.port.on("open", () => {
+				console.log('[Serial] Checking Baudrate');
+				setTimeout(() => {
+					if (!this.isDataReceived) {
+						if (this.baudratesUsedIndex + 1 < this.baudrates.length) {
+							this.baudratesUsedIndex++;
+							this.port.update({ baudRate: this.baudrates[this.baudratesUsedIndex] });
+							this.port.flush();
+							this.parser._flush((x) => { });
+							setTimeout(() => {
+								this.port.close();
+								setTimeout(() => { this.port.open(); }, 200);
+							}, 200);
+						}
+					} else {
+						console.log('[Serial] Opened');
 					}
-				} else {
-					console.log('[Serial] Opened');
-				}
-			}, 1000);
-		});
+				}, 1000);
+			});
 
-		this.port.on('error', function (err) {
-			PrinterWorker.socket.emit("error", "[Error] || " + err.message);
-			console.log('Error: ', err.message)
-		})
+			this.port.on('error', function (err) {
+				PrinterWorker.socket.emit("error", "[Error] || " + err.message);
+				console.log('Error: ', err.message)
+			});
 
-		this.parser.on('data', data => {
-			if (!this.isDataReceived && data == "start") this.isDataReceived = true;
-			if (data == "ok") this.commandEnquedCount--;
-			PrinterWorker.socket.emit("log", "[Data] >> " + data);
-			console.log('[Data] >>', data);
-		});
+			this.parser.on('data', data => {
+				if (!this.isDataReceived && data == "start") this.isDataReceived = true;
+				if (data == "ok") this.commandEnquedCount--;
+				PrinterWorker.socket.emit("log", "[Data] >> " + data);
+				console.log('[Data] >>', data);
+			});
+		}
 	}
 
 	serialWrite(data) {
@@ -159,10 +170,38 @@ class PrinterWorker {
 			}
 		)
 	}
-	PermissionProxy(apiKey,callback){
+
+	fileTransfer(data){
+		if(this.state == "idle"){
+			PrinterWorker.stream = socketStream.createStream();
+			socketStream(PrinterWorker.socket).emit(data,PrinterWorker.stream)
+			PrinterWorker.stream.pipe(fs.createWriteStream('printJob.gco'));
+		}
+	}
+
+	startPrintJob(data){
+		if(this.state == "idle"){
+			this.state = "printing"
+			this.serialWrite("M155 S5");
+			const liner = new lineByLine('printJob.gco');
+			let line;
+			while (line = liner.next()) {
+				while(this.commandEnquedCount >= 5){}
+				this.serialWrite(line);
+			}
+			while(this.commandEnquedCount == 0){}
+			this.state == "idle"
+		}
+	}
+
+	portState() {
+		PrinterWorker.socket.emit("state", this.state);
+	}
+
+	PermissionProxy(apiKey, callback) {
 		if (bcrypt.compareSync(apiKey, PrinterWorker.apiKey)) {
 			callback()
-		}else{
+		} else {
 			console.log("[SEC] >> Wrong Api KEY")
 		}
 	}
